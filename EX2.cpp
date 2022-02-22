b6a06c650378d7ab17a9dee45488c22acee6b8e5
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-22 13:57:58 +0300

    Add files via upload

---
+++ EX2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void reverse(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    for (int i = 0; i < 10 / 2; ++i)
+    {
+        reverse((array + (10 - i - 1)), (array + i));
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << *(array + i) << " ";
+    }
+
+    return 0;
+}
