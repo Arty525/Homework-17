c9866b7106b5290d26a677cd1de4ed53457e0614
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-22 15:26:51 +0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+bool sub(char* s, char* n) {
+	for (int i = 0; *(s+i) != '\0'; ++i) {
+
+		if (*(s+i) == *n) {
+			for (int j = 0; *(s+(i+j)) == *(n+j); ++j) {
+
+				if (*(n+j) == '\0') {
+					return true;
+				}
+
+			}
+		}
+
+	}
+}
+
+int main() {
+	char* a = "Hello world!";
+	char* b = "llo";
+	char* c = "Hi";
+
+	cout << sub(a, b) << " " << sub(a, c);
+}
